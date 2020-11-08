705af16adbe8066596069a8cfa5429e20693ec6e
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:18:55 -0300

    update embed messages

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,34 @@
-exports.run = async(bot, message, args) => {
+exports.run = async (bot, message, args) => {
     m = await message.channel.send({
         embed: {
             color: 3447003,
-            description: `🏓 Ping?`
-        }
+            title: "Ping",
+            description: `🏓 Ping?`,
+            timestamp: new Date(),
+            footer: {
+                text: "by Bravanzin",
+                icon_url:
+                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
+            },
+        },
     });
     m.edit({
         embed: {
             color: 3447003,
-            description: `🏓 Pong! A latência é ${m.createdTimestamp - message.createdTimestamp}ms.`
-        }
+            title: "Ping",
+            description: `🏓 Pong! A latência é ${
+                m.createdTimestamp - message.createdTimestamp
+            }ms.`,
+            timestamp: new Date(),
+            footer: {
+                text: "by Bravanzin",
+                icon_url:
+                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
+            },
+        },
     });
-}
+};
 
 exports.info = {
-    name: "ping"
-}
+    name: "ping",
+};
