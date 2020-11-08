9eefb10b8a5591a7f4dce031246e2dfdf5b59a58
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-07 22:19:04 -0300

    function play

---
+++ include/play.js
@@ -0,0 +1,56 @@
+const ytdlDiscord = require("ytdl-core-discord");
+const ytdl = require("ytdl-core");
+
+module.exports = {
+    async play(song, message) {
+        const queue = message.client.queue.get(message.guild.id);
+
+        if (!song) {
+            queue.channel.leave();
+            message.client.queue.delete(message.guild.id);
+            return queue.textChannel.send("🚫 A fila de musicas acabou.");
+        }
+
+        let stream = null;
+        let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
+
+        try {
+            stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
+        } catch (error) {
+            if (queue) {
+                queue.songs.shift();
+                module.exports.play(queue.songs[0], message);
+            }
+
+            console.error(error);
+            return message.channel.send(
+                `Error: ${error.message ? error.message : error}`
+            );
+        }
+
+        queue.connection.on("disconnect", () =>
+            message.client.queue.delete(message.guild.id)
+        );
+
+        const dispatcher = queue.connection
+            .play(stream, { type: streamType })
+            .on("finish", () => {
+                if (queue.loop) {
+                    //* se o "loop" estiver on, a musica vai voltar para o final da fila, assim repetindo infinitamente
+                    let lastSong = queue.songs.shift();
+                    queue.songs.push(lastSong);
+                    module.exports.play(queue.songs[0], message);
+                } else {
+                    //* seguindo a fila de musicas normalmente
+                    queue.songs.shift();
+                    module.exports.play(queue.songs[0], message);
+                }
+            })
+            .on("error", (err) => {
+                console.error(err);
+                queue.songs.shift();
+                module.exports.play(queue.songs[0], message);
+            });
+        dispatcher.setVolumeLogarithmic(queue.volume / 100);
+    },
+};
