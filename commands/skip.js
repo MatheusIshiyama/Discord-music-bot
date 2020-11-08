fd0c85a9bd8802019a164be062b57b4de4624c8d
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:55:17 -0300

    update embed messages

--- commands/skip.js
+++ commands/skip.js
@@ -1,14 +1,20 @@
+const { embedSend, embedReply } = require("../include/messages");
+
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) {
-      return message.reply("Não estou tocando nada para que eu possar passar de música");
+        return embedReply(
+            "Skip",
+            "Não estou tocando nada para que eu possar passar de música",
+            message
+        );
     }
 
     queue.playing = true;
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏭ passou de música`);
-}
+    return embedSend("Skip", `${message.author} ⏭ passou de música`, message);
+};
 
 exports.info = {
-    name: "skip"
-}
+    name: "skip",
+};
