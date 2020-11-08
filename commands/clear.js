8ec58799e5b600f5003f72a98a4e1775a7a91829
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:33:26 -0300

    update embed messages

--- commands/clear.js
+++ commands/clear.js
@@ -1,35 +1,13 @@
+const { embedSend, embedReply } = require('../include/messages');
+
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) {
-        return message.reply({
-            embed: {
-                color: 3447003,
-                title: "Clear",
-                description: "Não estou tocando nada",
-                timestamp: new Date(),
-                footer: {
-                    text: "by Bravanzin",
-                    icon_url:
-                        "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-                },
-            },
-        });
+        return embedReply("Clear", "Não estou tocando nada", message );
     }
 
     queue.songs = [];
-    queue.textChannel.send({
-        embed: {
-            color: 3447003,
-            title: "Help",
-            description: `${message.author} limpou a fila de músicas.`,
-            timestamp: new Date(),
-            footer: {
-                text: "by Bravanzin",
-                icon_url:
-                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-            },
-        },
-    });
+    return embedSend("Clear", `${message.author} limpou a fila de músicas.`, message);
 };
 
 exports.info = {
