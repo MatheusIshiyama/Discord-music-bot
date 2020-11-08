0cd422b9e972275f0e603e5f36a0ceb7007ee0b6
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-07 22:57:54 -0300

    command skip

--- commands/skip.js
+++ commands/skip.js
@@ -1,7 +1,7 @@
 exports.run = async () => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue)
-      return message.reply("Não estou tocando nada para que eu possar passar de música")ç
+      return message.reply("Não estou tocando nada para que eu possar passar de música");
 
     queue.playing = true;
     queue.connection.dispatcher.end();
