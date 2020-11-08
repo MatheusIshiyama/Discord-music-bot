049d2355efd694732673d7441248a74180f23e48
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:40:37 -0300

    update embed messages

--- commands/pause.js
+++ commands/pause.js
@@ -1,37 +1,19 @@
+const { embedSend, embedReply } = require("../include/messages");
+
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) {
-        return message.reply({
-            embed: {
-                color: 3447003,
-                title: "Pause",
-                description: "Não estou tocando nada",
-                timestamp: new Date(),
-                footer: {
-                    text: "by Bravanzin",
-                    icon_url:
-                        "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-                },
-            },
-        });
+        return embedReply("Pause", "Não estou tocando nada", message);
     }
 
     if (queue.playing) {
         queue.playing = false;
         queue.connection.dispatcher.pause(true);
-        return queue.textChannel.send({
-            embed: {
-                color: 3447003,
-                title: "Pause",
-                description: `${message.author} ⏸ pausou a música.`,
-                timestamp: new Date(),
-                footer: {
-                    text: "by Bravanzin",
-                    icon_url:
-                        "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-                },
-            },
-        });
+        return embedSend(
+            "Pause",
+            `${message.author} ⏸ pausou a música.`,
+            message
+        );
     }
 };
 
