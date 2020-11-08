01603c0a71660b638b3573f1822edaa3da1e1aa2
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:54:27 -0300

    update embed messages

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,7 +1,9 @@
+const { embedSend, embedReply } = require("../include/messages");
+
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if(!queue) {
-        return message.reply("Não estou tocando nada");
+        return embedReply("Shuffle", "Não estou tocando nada", message);
     }
 
     let songs = queue.songs;
@@ -11,8 +13,7 @@ exports.run = async (bot, message, args) => {
     }
     queue.songs = songs;
     message.client.queue.set(message.guild.id, queue);
-    queue.textChannel.send(`${message.author} aleatorizou a ordem das músicas.`);
-    console.log(songs)
+    return embedSend("Shuffle", `${message.author} aleatorizou a ordem das músicas.`, message);
 }
 
 exports.info = {
