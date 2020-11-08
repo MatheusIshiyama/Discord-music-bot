984e0ad85a2cc2f0d43948b462ec7e6b8b5e44ee
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-08 20:37:50 -0300

    update embed messages

--- commands/loop.js
+++ commands/loop.js
@@ -1,37 +1,17 @@
+const { embedSend, embedReply } = require("../include/messages");
+
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) {
-        return message.channel.send({
-            embed: {
-                color: 3447003,
-                title: "Loop",
-                description: "Não estou tocando nada",
-                timestamp: new Date(),
-                footer: {
-                    text: "by Bravanzin",
-                    icon_url:
-                        "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-                },
-            },
-        });
+        return embedReply("Loop", "Não estou tocando nada", message);
     }
 
     queue.loop = !queue.loop;
-    return queue.textChannel.send({
-        embed: {
-            color: 3447003,
-            title: "Loop",
-            description: `O loop está ${
-                queue.loop ? "**ligado**" : "**desligado**"
-            }`,
-            timestamp: new Date(),
-            footer: {
-                text: "by Bravanzin",
-                icon_url:
-                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-            },
-        },
-    });
+    return embedSend(
+        "Loop",
+        `O loop está ${queue.loop ? "**ligado**" : "**desligado**"}`,
+        message
+    );
 };
 
 exports.info = {
