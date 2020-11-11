50317c1b4ba121ca55fab43c0de8ffc369d34e3c
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-10 23:20:32 -0300

    update embed message

--- commands/queue.js
+++ commands/queue.js
@@ -1,29 +1,36 @@
+const { MessageEmbed } = require("discord.js");
 const { embedSend, embedReply } = require("../include/messages");
 
 exports.run = async (bot, message, args) => {
     const queue = message.client.queue.get(message.guild.id);
-    if(!queue) {
+    if (!queue) {
         return embedReply("Queue", "Não estou tocando nada", message);
     }
 
     const queueCount = queue.songs.length;
-    let msg;
 
-    msg = `**Tocando:** \`${queue.songs[0].title}\`\n **Próximas:**`;
-    if (queueCount < 10) {
-        for(i = 1; i < queueCount; i++) {
-            msg += `\n **${i}.** \`${queue.songs[i].title}\``;
-        }
-    } else {
-        for(i = 1; i <= 10; i++) {
-            msg += `\n **${i}.** \`${queue.songs[i].title}\``;
-        }
-        msg += `\n \`e mais ${queueCount-10}\``;
-    }
+    let queueEmbed = new MessageEmbed()
+        .setTitle(`**Queue**`)
+        .setDescription(`${queueCount} músicas na fila`)
+        .setColor("3498DB");
 
-    embedSend("Queue", msg, message);
-}
+    queue.songs.map((song, index) => {
+        if (index === 0) {
+            queueEmbed.addField(`Tocando: ${song.title}`, song.url);
+        } else if ( 0 < index && index < 10) {
+            queueEmbed.addField(song.url, `${index + 1}. ${song.title}`);
+        } else if (index === 10) {
+            queueEmbed.addField(`e mais ${queueCount - index} músicas na fila`, `by Bravanzin` );
+            return;
+        } else {
+            return;
+        }
+    });
+
+    message.channel.send(queueEmbed);
+
+};
 
 exports.info = {
-    name: "queue"
-}
+    name: "queue",
+};
