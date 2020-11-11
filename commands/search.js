e55a314ba1b7a236078b53323c3425a3b0d3335b
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-10 22:33:55 -0300

    command search

---
+++ commands/search.js
@@ -0,0 +1,58 @@
+const Youtube = require("simple-youtube-api");
+const { MessageEmbed } = require("discord.js");
+try {
+    const config = require("../config.json");
+    youtubeKey = config.youtubeKey;
+} catch (error) {
+    youtubeKey = process.env.YOUTUBE_KEY;
+}
+const youtube = new Youtube(youtubeKey);
+
+exports.run = async (bot, message, args) => {
+    //* verificar se há link de vídeo
+    if (!args.length) {
+        return embedReply("Play", `Use ${prefix}play <Youtube URL>`, message);
+    }
+
+    let resultsEmbed = new MessageEmbed()
+        .setTitle(`**Reply with the song number you want to play**`)
+        .setDescription(`Results for: ${args}`)
+        .setColor("3498DB");
+
+    try {
+        const results = await youtube.searchVideos(args, 10);
+        results.map((video, index) =>
+            resultsEmbed.addField(video.url, `${index + 1}. ${video.title}`)
+        );
+
+        let resultsMessage = await message.channel.send(resultsEmbed);
+
+        function filter(msg) {
+            const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/g;
+            return pattern.test(msg.content);
+        }
+
+        message.channel.activeCollector = true;
+        const response = await message.channel.awaitMessages(filter, {
+            max: 1,
+            time: 30000,
+            errors: ["time"],
+        });
+
+        const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+        console.log(choice);
+        message.client.commands.get("play").run(null, message, choice);
+
+        message.channel.activeCollector = false;
+        resultsMessage.delete().catch(console.error);
+        response.first().delete().catch(console.error);
+    } catch (error) {
+        console.error(error);
+        message.channel.activeCollector = false;
+        message.reply(error.message).catch(console.error);
+    }
+};
+
+exports.info = {
+    name: "search",
+};
