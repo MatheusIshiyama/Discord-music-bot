0c2b0bd99644bbf8f5243c33d38856b1bd72eefc
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-13 14:59:33 -0300

    fix permissions

--- commands/join.js
+++ commands/join.js
@@ -1,4 +1,22 @@
 exports.run = (bot, message, args) => {
+    //* verificar se quem solicitou está em um chat de voz
+    if (!channel) {
+        return embedReply(
+            "Play",
+            "Entre em algum chat de voz para que eu possa entrar",
+            message
+        );
+    }
+
+    //* verifcar permissões
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT")) {
+        return embedReply(
+            "Play",
+            "Não tenho permissão para entrar no chat de voz",
+            message
+        );
+    }
     message.member.voice.channel.join();
 };
 
