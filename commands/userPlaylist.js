7b2f30ba6c7c86ff5079d2360e63287cc7b70a67
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-20 16:56:34 -0300

    identation

--- commands/userPlaylist.js
+++ commands/userPlaylist.js
@@ -1,7 +1,7 @@
-const userModel = require('../models/user');
-const guildModel = require('../models/guild');
-const { MessageEmbed } = require('discord.js');
-const { embedReply } = require('../include/messages');
+const userModel = require("../models/user");
+const guildModel = require("../models/guild");
+const { MessageEmbed } = require("discord.js");
+const { embedReply } = require("../include/messages");
 
 exports.run = async (bot, message, args) => {
     const userReq = await userModel.findOne({ userId: message.author.id });
@@ -11,18 +11,28 @@ exports.run = async (bot, message, args) => {
         .setTitle(`User Playlist`)
         .setColor("3498DB");
 
-    if(!args) {
-        if(!userReq.playlist) {
-            return embedReply("User Playlist", `use \`${guildReq.prefix}up add\`, to add a playlist link`, message);
+    if (!args) {
+        if (!userReq.playlist) {
+            return embedReply(
+                "User Playlist",
+                `use \`${guildReq.prefix}up add\`, to add a playlist link`,
+                message
+            );
         } else {
-            return embedReply("User Playlist", userReq.playlist, message)
+            return embedReply("User Playlist", userReq.playlist, message);
         }
-    } else if (args == 'add') {
-        resultsEmbed.addField('Digite o link da playlist', 'digite \`cancel\` se quiser cancelar.');
+    } else if (args == "add") {
+        resultsEmbed.addField(
+            "Digite o link da playlist",
+            "digite `cancel` se quiser cancelar."
+        );
         const reply = await message.channel.send(resultsEmbed);
         function filter(msg) {
-            return msg.content.includes('cancel') || msg.content.includes('youtube.com/playlist');
-        };
+            return (
+                msg.content.includes("cancel") ||
+                msg.content.includes("youtube.com/playlist")
+            );
+        }
         message.channel.ativeCollector = true;
         const response = await message.channel.awaitMessages(filter, {
             max: 1,
@@ -32,19 +42,23 @@ exports.run = async (bot, message, args) => {
         message.channel.activeCollector = false;
         reply.delete();
         let content;
-        response.map(msg => {
+        response.map((msg) => {
             content = msg.content;
-        })
-        if(content == 'cancel') {
+        });
+        if (content == "cancel") {
             embedReply("User Playlist", "Operação cancelada", message);
         } else {
-            embedReply("User Playlist", `playlist solicitada ${content}`, message);
+            embedReply(
+                "User Playlist",
+                `playlist solicitada ${content}`,
+                message
+            );
         }
-    } else if (args == 'remove') {
+    } else if (args == "remove") {
         return embedReply("User Playlist", args, message);
     }
-}
+};
 
 exports.info = {
-    name: "up"
-}
+    name: "up",
+};
