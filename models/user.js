7dfa0452bc47a5fe485bd1026f2756c7f4ec71f9
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-21 17:20:23 -0300

    update user schema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,25 @@
 const { Schema, model } = require('mongoose');
 
 const User = Schema({
-    userId: String,
-    userName: String,
-    playlist: String,
-    favSong: String
+    user: {
+        id: String,
+        name: String
+    },
+    playlist: {
+        title: String,
+        url: String,
+        thumbnail: String
+    },
+    favSong: {
+        title: String,
+        url: String,
+        thumbnail: String
+    },
+    toDo: {
+        status: Boolean,
+        title: String,
+        desc: String
+    }
 })
 
 module.exports = model('User', User);
