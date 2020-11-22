113e3dcc70c3b43a1119bee49e2cc32b8f419bc3
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 20:11:54 -0300

    implement locales and new embed message

--- commands/queue.js
+++ commands/queue.js
@@ -1,26 +1,39 @@
+const userModel = require('../models/user');
+const messageEmbed = require('../include/messageEmbed');
 const { MessageEmbed } = require("discord.js");
-const { embedSend, embedReply } = require("../include/messages");
 
 exports.run = async (bot, message, args) => {
+    const userReq = await userModel.findOne({ id: message.author.id });
+    const localeQueue = require(`../locales/${userReq.locale}.json`);
+    const locale = localeQueue.queue;
+
     const queue = message.client.queue.get(message.guild.id);
+    messageEmbed.setTitle("Queue");
+
     if (!queue) {
-        return embedReply("Queue", "Não estou tocando nada", message);
+        messageEmbed.setDescription(locale.queue);
+        return message.channel.send(messageEmbed);
     }
 
     const queueCount = queue.songs.length;
 
     let queueEmbed = new MessageEmbed()
-        .setTitle(`**Queue**`)
-        .setDescription(`${queueCount} músicas na fila`)
-        .setColor("3498DB");
+        .setTitle(`Queue`)
+        .setDescription(`${queueCount} ${locale.queueDesc}`)
+        .setColor("3498DB")
+        .setThumbnail(
+            "https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512"
+        )
+        .setTimestamp(new Date())
+        .setFooter("by Bravanzin", "https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512");
 
     queue.songs.map((song, index) => {
         if (index === 0) {
-            queueEmbed.addField(`Tocando: ${song.title}`, song.url);
-        } else if ( 0 < index && index < 10) {
-            queueEmbed.addField(song.url, `${index + 1}. ${song.title}`);
-        } else if (index === 10) {
-            queueEmbed.addField(`e mais ${queueCount - index} músicas na fila`, `by Bravanzin` );
+            queueEmbed.addField(`${locale.playing} ${song.title}`, song.url);
+        } else if ( 0 < index && index < 11) {
+            queueEmbed.addField(song.url, `${index}. ${song.title}`);
+        } else if (index === 11) {
+            queueEmbed.addField(`${locale.size[0]} ${queueCount - index} ${locale.size[1]}`, locale.enjoy );
             return;
         } else {
             return;
