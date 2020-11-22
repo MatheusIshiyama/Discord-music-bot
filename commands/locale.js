e99601bf9f7b801736295b61aea0833b16118ef8
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 13:56:54 -0300

    command locale

---
+++ commands/locale.js
@@ -0,0 +1,46 @@
+const userModel = require("../models/user");
+const guildModel = require('../models/guild');
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+    const userReq = await userModel.findOne({ id: message.author.id });
+    const guildReq = await guildModel.findOne({ serverId: message.guild.id });
+    const { locale } = require(`../locales/${userReq.locale}.json`);
+
+    const change = locale.change;
+
+    const localeInfo = new MessageEmbed()
+        .setTitle(`Locale`)
+        .setColor("3498DB")
+        .setThumbnail("https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512")
+        .setTimestamp(new Date())
+        .setFooter(`by ${bot.user.username}`, bot.user.avatarURL());
+
+    if (!args) {
+        localeInfo.setDescription(locale.support);
+        localeInfo.addField(locale.current, change[0] + `\`${guildReq.prefix}` + change[1]);
+        message.channel.send(localeInfo);
+    } else if(args === "pt-br") {
+        if(userReq.locale === "pt-br") {
+            localeInfo.setDescription("O idioma atual é `Português pt-br`");
+            message.channel.send(localeInfo);
+        } else {
+            await userModel.findOneAndUpdate({ id: message.author.id }, { locale: "pt-br" });
+            localeInfo.setDescription("O idioma atual foi alterado para `Português \"pt-br\"`");
+            message.channel.send(localeInfo);
+        }
+    } else if(args === "en-us") {
+        if(userReq.locale === "en-us") {
+            localeInfo.setDescription("The current language is `English en-us`")
+            message.channel.send(localeInfo);
+        } else {
+            await userModel.findOneAndUpdate({ id: message.author.id }, { locale: "en-us" });
+            localeInfo.setDescription("The current language was set to `English \"en-us\"`");
+            message.channel.send(localeInfo);
+        }
+    }
+};
+
+exports.info = {
+    name: "locale",
+};
