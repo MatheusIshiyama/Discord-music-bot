93ead6a03fa11c17f58bae4688312153b6a43f03
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 16:43:02 -0300

    implement locales and new embed message

--- commands/ping.js
+++ commands/ping.js
@@ -1,32 +1,14 @@
+const userModel = require("../models/user");
+const messageEmbed = require("../include/messageEmbed");
+
 exports.run = async (bot, message, args) => {
-    m = await message.channel.send({
-        embed: {
-            color: 3447003,
-            title: "Ping",
-            description: `🏓 Ping?`,
-            timestamp: new Date(),
-            footer: {
-                text: "by Bravanzin",
-                icon_url:
-                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-            },
-        },
-    });
-    m.edit({
-        embed: {
-            color: 3447003,
-            title: "Ping",
-            description: `🏓 Pong! A latência é ${
-                m.createdTimestamp - message.createdTimestamp
-            }ms.`,
-            timestamp: new Date(),
-            footer: {
-                text: "by Bravanzin",
-                icon_url:
-                    "https://cdn.discordapp.com/app-icons/690359745420591415/8ca3f1829ce42cc9935bd562c3ead3f9.png",
-            },
-        },
-    });
+    const userReq = await userModel.findOne({ id: message.author.id });
+    const { ping } = require(`../locales/${userReq.locale}.json`);
+
+    messageEmbed.setTitle("Ping").setDescription("Ping?");
+    m = await message.channel.send(messageEmbed);
+    messageEmbed.setDescription(`Pong! ${ping} \`${Date.now() - message.createdTimestamp}ms.\``)
+    m.edit(messageEmbed);
 };
 
 exports.info = {
