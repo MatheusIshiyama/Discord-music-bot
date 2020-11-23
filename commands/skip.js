c8a9ecbdb4f838582de72890d4cfe5a3a04f4258
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 21:14:51 -0300

    implement locales and new embed message

--- commands/skip.js
+++ commands/skip.js
@@ -1,18 +1,22 @@
-const { embedSend, embedReply } = require("../include/messages");
+const userModel = require('../models/user');
+const messageEmbed = require('../include/messageEmbed');
 
 exports.run = async (bot, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
-    if (!queue) {
-        return embedReply(
-            "Skip",
-            "Não estou tocando nada para que eu possar passar de música",
-            message
-        );
-    }
+    const userReq = await userModel.findOne({ id: message.author.id });
+    const { skip } = require(`../locales/${userReq.locale}.json`);
 
-    queue.playing = true;
-    queue.connection.dispatcher.end();
-    return embedSend("Skip", `${message.author} ⏭ passou de música`, message);
+    const queue = message.client.queue.get(message.guild.id);
+    messageEmbed.setTitle("Skip");
+
+    if (!queue) {
+        messageEmbed.setDescription(skip.queue);
+        return message.channel.send(messageEmbed);
+    } else {   
+        queue.playing = true;
+        queue.connection.dispatcher.end();
+        messageEmbed.setDescription(`${message.author.username} ${skip.skipped}`);
+        message.channel.send(messageEmbed);
+    }
 };
 
 exports.info = {
