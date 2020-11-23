87e4a401c2029548f43ae34961c0d4d57f54952f
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 21:06:51 -0300

    implement locales and new embed message

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,19 +1,27 @@
-const { embedSend, embedReply } = require("../include/messages");
+const userModel = require('../models/user');
+const messageEmbed = require('../include/messageEmbed');
 
 exports.run = async (bot, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
-    if(!queue) {
-        return embedReply("Shuffle", "Não estou tocando nada", message);
-    }
+    const userReq = userModel.findOne({ id: message.author.id });
+    const { shuffle } = require(`../locales/${userReq.locale}.json`);
 
-    let songs = queue.songs;
-    for (let i = songs.length - 1; i > 1; i--) {
-      let j = 1 + Math.floor(Math.random() * i);
-      [songs[i], songs[j]] = [songs[j], songs[i]];
+    const queue = message.client.queue.get(message.guild.id);
+    messageEmbed.setTitle("Shuffle");
+
+    if(!queue) {
+        messageEmbed.setDescription(shuffle.queue);
+        return message.channel.send(messageEmbed);
+    } else {   
+        let songs = queue.songs;
+        for (let i = songs.length - 1; i > 1; i--) {
+            let j = 1 + Math.floor(Math.random() * i);
+            [songs[i], songs[j]] = [songs[j], songs[i]];
+        }
+        queue.songs = songs;
+        message.client.queue.set(message.guild.id, queue);
+        messageEmbed.setDescription(`${message.author.username} ${shuffle.shuffled}`);
+        message.channel.send(messageEmbed);
     }
-    queue.songs = songs;
-    message.client.queue.set(message.guild.id, queue);
-    return embedSend("Shuffle", `${message.author} aleatorizou a ordem das músicas.`, message);
 }
 
 exports.info = {
