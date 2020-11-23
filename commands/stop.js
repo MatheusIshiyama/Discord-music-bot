f583b8f02a231f6f070474b0a2728ae02b18babd
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-22 21:20:24 -0300

    implement locales and new embed message

--- commands/stop.js
+++ commands/stop.js
@@ -1,14 +1,21 @@
-const { embedSend, embedReply } = require("../include/messages");
+const userModel = require('../models/user');
+const messageEmbed = require('../include/messageEmbed');
 
 exports.run = async (bot, message, args) => {
-    const queue = message.client.queue.get(message.guild.id);
-    if(!queue) {
-        return embedReply("Stop", "Não estou tocando nada", message);
-    }
+    const userReq = userModel.findOne({ id: message.author.id });
+    const { stop } = require(`../locales/${userReq.locale}.json`);
 
-    queue.songs = [];
-    queue.connection.dispatcher.end();
-    return embedSend("Stop", `${message.author} parou a música.`, message);
+    const queue = message.client.queue.get(message.guild.id);
+    messageEmbed.setTitle("Stop");
+    if(!queue) {
+        messageEmbed.setDescription(stop.queue);
+        return message.channel.send(messageEmbed);
+    } else {   
+        queue.songs = [];
+        queue.connection.dispatcher.end();
+        messageEmbed.setDescription(`${message.author.username} ${stop.stopped}`);
+        message.channel.send(messageEmbed);
+    }
 }
 
 exports.info = {
