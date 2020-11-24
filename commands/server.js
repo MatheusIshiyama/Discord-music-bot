21fa0ae269cb3b319160ef76f68e3617f6230e8a
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-23 23:27:46 -0300

    fix guild find id

--- commands/server.js
+++ commands/server.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 
 exports.run = async (bot, message, args) => {
     const userReq = await userModel.findOne({ id: message.author.id });
-    const guildReq = await guildModel.findOne({ serverId: message.guild.id });
+    const guildReq = await guildModel.findOne({ id: message.guild.id });
     const { server } = require(`../locales/${userReq.locale}.json`);
 
     const msg = new MessageEmbed()
@@ -15,7 +15,7 @@ exports.run = async (bot, message, args) => {
             message.guild.iconURL() ?  message.guild.iconURL() : "https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512"
         )
         .addFields(
-            { name: `${server.server} \`${guildReq.serverName}\``, value: `${server.prefix} \`${guildReq.prefix}\`` },
+            { name: `${server.server} \`${guildReq.name}\``, value: `${server.prefix} \`${guildReq.prefix}\`` },
             { name: `${server.playlists.title}`, value: guildReq.playlists ? guildReq.playlists : server.playlists.desc }
             )
         .setTimestamp(new Date())
