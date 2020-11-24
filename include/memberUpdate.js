e2cdceae17b7b78d8888cc42f2d5cc9b96815fc3
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-23 23:13:51 -0300

    add locale

--- include/memberUpdate.js
+++ include/memberUpdate.js
@@ -3,9 +3,11 @@ const guildModel = require('../models/guild');
 module.exports = {
     async countUpdate(guild) {
         const guildInfo = await guildModel.findOne({ id: guild.id });
+        const localeApp = require(`../locales/${guildInfo.locale}.json`);
+        const locale = localeApp.app.mcount;
         if(guildInfo.mcountId != null) {
         const channel = await guild.channels.cache.get(guildInfo.mcountId);
-            channel.setName(`${guild.memberCount} usuarios`);
+            channel.setName(`${guild.memberCount} ${locale}`);
         }
     }
 }
