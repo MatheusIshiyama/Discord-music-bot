e9758a97299bf228cca18b1600476c4617440ea5
Matheus Ishiyama <semrumo3@hotmail.com>
2020-11-23 22:49:54 -0300

    fix find id and mcountId

--- include/memberUpdate.js
+++ include/memberUpdate.js
@@ -2,9 +2,9 @@ const guildModel = require('../models/guild');
 
 module.exports = {
     async countUpdate(guild) {
-        const guildInfo = await guildModel.findOne({ serverId: guild.id });
-        if(guildInfo.memberCountId != null) {
-        const channel = await guild.channels.cache.get(guildInfo.memberCountId);
+        const guildInfo = await guildModel.findOne({ id: guild.id });
+        if(guildInfo.mcountId != null) {
+        const channel = await guild.channels.cache.get(guildInfo.mcountId);
             channel.setName(`${guild.memberCount} usuarios`);
         }
     }
